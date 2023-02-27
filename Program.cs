da8cb10090f3164ff2bb5496189a00acb72167b5
Egor <agent-nino@bk.ru>
2023-02-27 19:57:22 +0300

    Add FUNCTION Final Version

--- Program.cs
+++ Program.cs
@@ -19,6 +19,25 @@ for (int i = 0; i < n; i++)
 }
 }
 
+string [] CheckArray(string [] array){
+
+//
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{if (array[i].Length < 4) 
+    j++; 
+}
+string [] newarray = new string [j];
+j = 0;
+for (int i = 0; i < array.Length; i++)
+{if (array[i].Length < 4) {
+    newarray[j] = array[i];
+    j++; }
+}
+return newarray;
+}
+
+
 
 System.Console.WriteLine("Итоговая проверочная работа");
 System.Console.Write("Введите колличество элементов массива: ");
@@ -29,3 +48,6 @@ string [] array = new string [a];
 System.Console.WriteLine("Эллементы массива через Enter");
 EnterArray(array,a);
 PrintArray(array);
+string [] newarray = CheckArray(array);
+System.Console.WriteLine("Ваш новый массив");
+PrintArray(newarray);
