488a37c3e57b664262c63a243b4ae05e7484be74
Egor <agent-nino@bk.ru>
2023-02-27 19:36:43 +0300

    BEGIN

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+ void PrintArray(int [] array){
+ System.Console.Write("[");
+int n = array.Length;
+if (n == 0) System.Console.WriteLine("]");
+ for (int i = 0; i < n; i++)
+{if(i == n-1)
+   System.Console.WriteLine($"{array[i]}]");
+ else System.Console.Write($"{array[i]}, ");
+ }
+}
+
+
+void EnterArray(int [] array, int n){
+   
+for (int i = 0; i < n; i++)
+{
+     array[i] = new Random().Next(100,1000);
+
+}
+}
+
+
+System.Console.WriteLine("Итоговая проверочная работа");
+System.Console.Write("Введите колличество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 0){System.Console.WriteLine("Ввели отрицательное число!!!");
+return;}
